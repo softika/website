f2b6956537bf5edbf131a9a801528810ab44e2b9
fmiskovic <fmiskovic@yandex.com>
2024-01-20 14:14:38 +0100

    fix padding 2

--- pages/index.vue
+++ pages/index.vue
@@ -25,22 +25,22 @@ import Tech from "~/components/Tech.vue";
   <v-row align="center" class="py-2" justify="center">
     <v-col cols="12"/>
   </v-row>
-  <v-row align="center" class="py-2" justify="center">
+  <v-row align="center" justify="center">
     <v-col cols="12">
       <ServiceWeb/>
     </v-col>
   </v-row>
-  <v-row align="center" class="py-2" justify="center">
+  <v-row align="center" justify="center">
     <v-col cols="12">
       <ServiceApi/>
     </v-col>
   </v-row>
-  <v-row align="center" class="py-2" justify="center">
+  <v-row align="center" justify="center">
     <v-col cols="12">
       <ServiceMobile/>
     </v-col>
   </v-row>
-  <v-row align="center" class="py-2" justify="center">
+  <v-row align="center" justify="center">
     <v-col cols="12">
       <ServiceSaas/>
     </v-col>
