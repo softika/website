a89d78a6beb793a4fdbc3c9bc3ece77b802c1cd9
fmiskovic <fmiskovic@proton.me>
2024-03-13 11:57:11 +0100

    robots.txt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,6 +38,7 @@ export default defineNuxtConfig({
                 config.plugins.push(vuetify({autoImport: true}))
             })
         },
+        '@nuxtjs/robots',
     ],
     vite: {
         vue: {
