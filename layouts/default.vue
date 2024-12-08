39666a393ee4df84a900976a80cb4cbbd1155635
fmiskovic <fmiskovic@proton.me>
2024-12-08 15:23:16 +0100

    update deps and install Vercel's speed insights

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 </script>
 
 <template>
   <v-app>
     <Header/>
     <v-main>
+      <SpeedInsights/>
       <v-container :fluid="true" class="w-100 h-100">
         <slot/>
       </v-container>
